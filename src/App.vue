7e395b72c4b781ff983d31fcd8c482d1ea50eb94
Luis Fernando Contreras Guerra <luisguerra495@gmail.com>
2018-11-24 16:52:14 -0600

    Removed the other components and add vuex action

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
+
+<script>
+export default {
+  beforeCreate() {
+    this.$store.dispatch('SET_FIRST_PRODUCTS')
+  }
+}
+</script>
+
 
 <style>
 #app {
