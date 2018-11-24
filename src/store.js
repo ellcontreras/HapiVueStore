ea7a3425e32ca33dc9c1d5c4bd0af1943c32e6b0
Luis Fernando Contreras Guerra <luisguerra495@gmail.com>
2018-11-24 16:52:47 -0600

    Created store with products data

--- src/store.js
+++ src/store.js
@@ -1,16 +1,32 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import Axios from 'axios';
 
 Vue.use(Vuex)
 
+const api_url = 'http://localhost:8081'
+
 export default new Vuex.Store({
   state: {
-
+    products: []
   },
   mutations: {
-
+    addProducts(state, data) {
+      state.products = data
+    },
+    addProduct(state, data) {
+      Axios.post(`${api_url}/api/products/insert`, data).then(res => {
+        state.products.push(res.data)
+      })
+    }
   },
   actions: {
+    SET_FIRST_PRODUCTS({ commit }) {
+      Axios.get(`${api_url}/api/products/all`).then(res => {
+        let data = res.data
 
+        commit('addProducts', data)
+      })
+    }
   }
 })
